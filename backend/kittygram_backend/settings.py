4aee5af97426dc01a0e3e212eb4c50d9d232eb78
Roman Inozemtsev <dev@koyote92.ru>
2023-05-17 21:59:36 +0000

    Update settings,py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG', False)
+# В общем автотесты ругаются, что тут не False, скинул тебе в пачку трейсбек
+DEBUG = False
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
 
